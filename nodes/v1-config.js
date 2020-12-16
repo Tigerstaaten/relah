facd44e8be402bce8684397ee495b20d5283918c
Tigerstaaten <116427361+Tigerstaaten@users.noreply.github.com>
2020-12-16 20:01:47 +0000

    test pod

--- nodes/v1-config.js
+++ nodes/v1-config.js
@@ -9,4 +9,4 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an 'AS IS' BASIS,
- * WITHOUT WARRANTIES OR C
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KI
