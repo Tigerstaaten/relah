5dd1aa087385221a235a901482d9f8b640f64542
Tigerstaaten <116427361+Tigerstaaten@users.noreply.github.com>
2023-03-07 21:29:18 +0000

    Fix flake8 warnings.

--- nodes/v1.js
+++ nodes/v1.js
@@ -5,3 +5,26 @@
  * Licensed under the Apache License, Version 2.0 (the 'License');
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an 'AS IS' BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+module.exports = function(RED) {
+  const debug = require('debug')('node-red-contrib-watson-machine-learning');
+  const request = require('request');
+
+  function start(msg, config) {
+    return Promise.resolve();
+  }
+
+  function checkForParameters(msg, config, m, params) {
+    var message = '';
+
+    if (!m || '' === m) {
+      message = 'Required mode has not been specified';
